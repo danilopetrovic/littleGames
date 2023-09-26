0839e9ea4f2b42690d49e705e9b635eaefed0be3
Danilo Petrovic <danilo86petrovic@gmail.com>
2023-09-26 06:33:02 +0200

    8 Multiple Pointers Count Unique Values Challenge & 10. Count Unique Values Solution

---
+++ _ALGORITHMS/10.1 unique_values.js
@@ -0,0 +1,18 @@
+// Works for sorted arrays and changes the array...
+// Time complexity O(n)
+// Space complexity O(1)
+function countUniqueValues(arr) {
+    if (arr.length === 0) return 0;
+    var i = 0;
+    for (var j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+    return i + 1;
+}
+
+countUniqueValues([1, 2, 2, 5, 7, 7, 99]);
+const result = countUniqueValues([1, 2, 2, 5, 7, 7, 99]);
+console.log(result);
