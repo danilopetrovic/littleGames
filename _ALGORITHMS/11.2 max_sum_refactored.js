9afcb61f7793ea7ba6d5278fb9f018253ebd2eb2
Danilo Petrovic <danilo86petrovic@gmail.com>
2023-09-28 23:22:43 +0200

    11. Sliding Window Pattern

---
+++ _ALGORITHMS/11.2 max_sum_refactored.js
@@ -0,0 +1,19 @@
+// Time complexity O(n)
+
+function maxSubarraySum(arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) return null;
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i]; // svaku iteraciju oduzme prvi i sledeci umesto da sve opet zbraja...
+        maxSum = Math.max(maxSum, tempSum); // uzima vecu sumu
+    }
+    return maxSum;
+}
+
+const result = maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3);
+console.log(result);
